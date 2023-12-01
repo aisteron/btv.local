b433183ec82bdee8599004a8fa059e167d4b0292
aisteron <timotheus@list.ru>
2023-12-02 01:08:11 +0300

    w02.12

--- src/assets/js/ui/index.js
+++ src/assets/js/ui/index.js
@@ -2,6 +2,7 @@ import  { qs,sw } from '../libs';
 
 export function Ui(){
 	swipers.services()
+	swipers.news()
 }
 
 export const swipers = {
@@ -36,6 +37,43 @@ export const swipers = {
         1024: {
           slidesPerView: 3,
           spaceBetween: 20,
+        },
+      },
+		}
+		sw.init(ss, settings)
+	},
+
+	async news(){
+
+		let ss = qs('.swiper.news')
+		if(!ss) return
+
+		await sw.load()
+
+		let settings = {
+			slidesPerView: 1,
+			spaceBetween: 10,
+			pagination: {
+				el: qs(".pagination",ss.closest('.widget')),
+				clickable: true,
+			},
+			navigation: {
+        nextEl: qs(".next",ss.closest(".widget")),
+        prevEl: qs(".prev",ss.closest(".widget")),
+      },
+			watchSlidesProgress: true,
+			breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
         },
       },
 		}
